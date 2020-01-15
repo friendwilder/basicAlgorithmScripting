d637359b22e8728cc4de4cb7e381df9e33724824
Wilder <friendwilder@yahoo.com.mx>
2020-01-15 09:12:33 -0600

    splitify added using join and split

---
+++ splitify.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Add your code below this line
+  var newString = str.split(/\W/);
+  return newString;
+
+  // Add your code above this line
+}
+console.log(splitify("Hello World,I-am code"));
